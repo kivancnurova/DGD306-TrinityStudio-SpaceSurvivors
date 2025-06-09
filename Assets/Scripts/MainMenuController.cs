fd18ac6ea6f72c0e657bde70acb3e87faa2f1292
Kıvanç Nurova <kivancnrv@gmail.com>
2025-06-10 00:52:47 +0300

    MainMenu added

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+        public void PlayGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+}
