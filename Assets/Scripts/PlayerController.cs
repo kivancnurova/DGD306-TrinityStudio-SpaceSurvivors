998b9a36a3d202c9a10b21a8c7a5c5523fe65407
Kıvanç Nurova <kivancnrv@gmail.com>
2025-06-10 03:04:41 +0300

    Mainmenu after player dies

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -73,6 +74,7 @@ public class PlayerController : MonoBehaviour
         if (playerStats.playerCurrentHealth <= 0f)
         {
             Destroy(gameObject);
+            SceneManager.LoadScene("MainMenu");
         }
     }
 
