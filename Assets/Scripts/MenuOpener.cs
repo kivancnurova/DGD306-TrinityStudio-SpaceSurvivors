8f57fe82650e0e605d9582ffc2bc273ea7b262a4
kivancnurova <kivancnrv@gmail.com>
2025-06-16 22:32:33 +0300

    Pause Game added

---
+++ Assets/Scripts/MenuOpener.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MenuOpener : MonoBehaviour
+{
+    public InputActionReference openMenuActionRef;
+
+    public GameObject menuPanel;
+
+    private void OnEnable()
+    {
+        if (openMenuActionRef != null)
+        {
+            openMenuActionRef.action.performed += OnOpenMenuPerformed;
+            openMenuActionRef.action.Enable();
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (openMenuActionRef != null)
+        {
+            openMenuActionRef.action.performed -= OnOpenMenuPerformed;
+            openMenuActionRef.action.Disable();
+        }
+    }
+
+    private void OnOpenMenuPerformed(InputAction.CallbackContext context)
+    {
+        OpenMenu();
+    }
+
+    private void OpenMenu()
+    {
+        if (menuPanel != null && Time.timeScale > 0f)
+        {
+            Time.timeScale = 0f;
+            menuPanel.SetActive(true);
+        }
+        else
+        {
+            Debug.LogWarning("Menu Panel referansı atanmamış veya zaman ölçeği zaten 0!");
+        }
+    }
+
+    public void ResumeGame()
+    {
+        if (menuPanel != null)
+        {
+            Time.timeScale = 1f;
+            menuPanel.SetActive(false);
+        }
+        else
+        {
+            Debug.LogWarning("Menu Panel referansı atanmamış!");
+
+        }
+    }
+
+}
